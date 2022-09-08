5d842b372d50bba5e65d198cce52e980976bd696
Liyu-ko <liyako062605@gmail.com>
2022-09-08 08:40:54 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print size of character
+ *
+ * Description: using main function
+ * this program prints the size of character
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	printf("size of an int: %ld byte(s)\n", sizeof(i));
+	printf("size of a long: %ld byte(s)\n", sizeof(li));
+	printf("size of a lon long int: %ld byte(s)\n", sizeof(lli));
+	printf("size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
