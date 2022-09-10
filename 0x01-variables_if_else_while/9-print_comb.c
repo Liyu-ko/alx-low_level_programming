0769cd1f178cf46190f584c0e6c0c0b7c1fd7859
Liyu-ko <liyako062605@gmail.com>
2022-09-10 09:22:36 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - combinaation of single digit number
+ *
+ *
+ *
+ * Return:0 when sucess
+ */
+int main(void)
+{
+int i = '0';
+while (i != '9')
+{
+	putchar(i);
+	if (i != '9')
+	{
+		putchar(',');
+		putcchar(' ');
+	}
+	i++;
+}
+putchar('\n');
+return(0);
+}
